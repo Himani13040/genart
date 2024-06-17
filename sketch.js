01ee944553828da0dc7b9345df555b7a0ae79a41
Himani <himani13040@iiitd.ac.in>
2024-06-17 14:33:09 +0530

    Setup canvas-sketch

---
+++ sketch.js
@@ -0,0 +1,23 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'pink';
+    context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width/2, height/2, 300, 0, Math.PI * 2, false);
+    context.fillStyle = 'violet';
+    context.fill();
+
+    context.lineWidth = 30;
+    context.strokeStyle = 'purple';
+    context.stroke();
+  };
+};
+
+canvasSketch(sketch, settings);
